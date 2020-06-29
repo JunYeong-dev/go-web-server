4c84afe90db746d4c313e6b2d31e75299d287f62
Jun-cloud <kjy8827@gmail.com>
2020-06-29 22:09:31 +0900

    Create go server - echo

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func handleHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
